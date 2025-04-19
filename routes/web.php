d1c1ddd37b1e6fbdcac5be5b726b3a771fb0f183
amlanjlahkar <amlanjlahkar@protonmail.com>
2025-04-19 17:10:19 +0530

    feat: update global routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('ui/home');
+});
+
+Route::get('/internal/login', function () {
+    return view('auth/internal/login');
+});
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.show');
