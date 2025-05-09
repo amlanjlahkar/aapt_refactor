d5c96b327b687c778e20948a52c058af9275a60e
amlanjlahkar <amlanjlahkar@protonmail.com>
2025-05-09 21:47:24 +0530

    feat(view): add an alternate footer

---
+++ resources/views/partials/footer-alt.blade.php
@@ -0,0 +1,28 @@
+<footer
+    class="w-full border-t-2 border-gray-500 bg-gray-800 pt-6 pb-6 text-gray-100"
+>
+    <div class="mx-auto max-w-7xl px-4 text-sm">
+        <p class="mb-2 text-center text-gray-300">
+            Website is Designed, Developed and Hosted by National Informatics
+            Centre,Ministry of Electronics & IT (MeitY) | Government of India
+        </p>
+        <hr class="mt-4 mb-4 border-gray-600" />
+        <div
+            class="flex flex-col items-start justify-between space-y-2 text-center md:flex-row md:items-center md:space-y-0 md:text-left"
+        >
+            <div class="text-xs text-gray-400">
+                © 2025 Assam APT. All Rights Reserved.
+            </div>
+            <div class="space-x-4 text-xs text-gray-400">
+                <span>
+                    Visitors:
+                    <strong>Count Unavailale</strong>
+                </span>
+                <span>
+                    Last Updated:
+                    <strong>{{ now()->format('F d, Y') }}</strong>
+                </span>
+            </div>
+        </div>
+    </div>
+</footer>
