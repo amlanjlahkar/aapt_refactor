01c7e829a38463ea9156ff356ccd5488cf98b8b4
amlanjlahkar <amlanjlahkar@protonmail.com>
2025-05-24 01:15:29 +0530

    'Super Admin' Role passes all permission checks

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider {
     public function boot(): void {
         Blade::directive('up', function ($expression) {
             return "<?php echo Str::upper($expression); ?>";
+        });
+
+        // Implicitly grant "Super Admin" role all permissions
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
         });
     }
 }
